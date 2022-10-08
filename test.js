fe13c497ac348357a03f8614493116a8edd7f565
Marc Abillama <mabillama@gmail.com>
2022-10-08 13:31:06 +0200

    Creates unique keys for local storage

--- test.js
+++ test.js
@@ -16,17 +16,9 @@
 // var h = document.getElementById("hello");
 // // h.onclick = console.log("Clicked!");
 // h.addEventListener("click", clicked());
-
 // export { clicked };
 
-function clicked() {
-  console.log("clicked!");
-  window.localStorage.setItem("name", "suzan");
-  updateUI();
-}
-
-const button = document.getElementById("hello");
-button.addEventListener("click", clicked);
+updateUI();
 
 if (typeof Storage != undefined) {
   console.log("It stores!");
@@ -42,5 +34,46 @@ function updateUI() {
   }
 
   console.log(values);
-  document.getElementById("ls-content").textContent = values[1];
+  document.getElementById("ls-content").textContent = values;
 }
+
+function updateUIreload() {
+  let values = [];
+  let keys = Object.keys(localStorage);
+  let i = keys.length;
+
+  while (i--) {
+    values.push(localStorage.getItem(keys[i]));
+  }
+
+  console.log(values);
+  const content = document.getElementById("ls-content");
+
+  const fragment = document.createDocumentFragment();
+  const li = content.appendChild(document.createElement("p"));
+  li.textContent = values;
+}
+
+/* 
+Button functions
+  - CHECK it needs to get the input field to put to localStorage
+  - CHECK it needs to generate a random key *using date* @todo develop to use uuid
+  - it needs to add to localStorage with a link
+  - validation for links
+*/
+function clicked() {
+  // console.log("clicked!");
+  window.localStorage.setItem(
+    generateKey(),
+    document.getElementById("link-box").value
+  );
+  updateUIreload();
+}
+
+function generateKey() {
+  return Date.now();
+  // console.log(Date.now());
+}
+
+const button = document.getElementById("hello");
+button.addEventListener("click", clicked);
