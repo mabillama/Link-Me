3c9222c27529e5e8b778bc18fb31410e41563fa3
Marc Abillama <mabillama@gmail.com>
2022-10-08 15:00:17 +0200

    Adds basic validation. input length > 13 and url regex

--- test.js
+++ test.js
@@ -1,29 +1,11 @@
-// import linker from "./linker.json" assert { type: "json" };
-
-// console.log(linker);
-// const objStr = JSON.stringify(linker);
-// console.log(obj);
-// const obj = JSON.parse(objStr);
-// const linkerStr = linker.JSON.parse();
-// console.log(linkerStr);
-
-// document.getElementById("over-here").innerHTML = obj;
-
-// function clicked() {
-//   console.log("Clicked!");
-// }
-
-// var h = document.getElementById("hello");
-// // h.onclick = console.log("Clicked!");
-// h.addEventListener("click", clicked());
-// export { clicked };
-
 updateUI();
 
+// localStorage functionality test
 if (typeof Storage != undefined) {
   console.log("It stores!");
 }
 
+// RELOADS UI
 function updateUI() {
   let values = [];
   let keys = Object.keys(localStorage);
@@ -43,11 +25,13 @@ function updateUI() {
     // const fragment = document.createDocumentFragment();
     let li1 = content1.appendChild(document.createElement("p"));
     li1.textContent = el;
-    console.log(li1);
-    console.log(el);
+    // console.log(li1);
+    // console.log(el);
   });
+  document.getElementById("link-box").value = "";
 }
 
+// RELOADS UI
 function updateUIreload() {
   let values = [];
   let keys = Object.keys(localStorage);
@@ -57,7 +41,7 @@ function updateUIreload() {
     values.push(localStorage.getItem(keys[i]));
   }
 
-  console.log(values);
+  // console.log(values);
   const content = document.getElementById("ls-content");
 
   const fragment = document.createDocumentFragment();
@@ -65,8 +49,7 @@ function updateUIreload() {
   li.textContent = values;
 }
 
-/* 
-Button functions
+/* Button functions
   - CHECK it needs to get the input field to put to localStorage
   - CHECK it needs to generate a random key *using date* @todo develop to use uuid
   - it needs to add to localStorage with a link
@@ -75,17 +58,37 @@ Button functions
     - can't be empty
 */
 function clicked() {
-  // console.log("clicked!");
-  window.localStorage.setItem(
-    generateKey(),
-    document.getElementById("link-box").value
-  );
-  updateUI();
+  const linkBoxText = document.getElementById("link-box").value;
+  let validLink = false;
+
+  // Basic validation: size and valid url
+  linkBoxText.length > 13 && validateUrl(linkBoxText)
+    ? (validLink = !validLink)
+    : null;
+
+  if (validLink) {
+    window.localStorage.setItem(
+      generateKey(),
+      document.getElementById("link-box").value
+    );
+    updateUI();
+  } else {
+    console.log("error");
+    updateUI();
+  }
 }
+
+// UTIL FUNCTIONS
 
 function generateKey() {
   return Date.now();
   // console.log(Date.now());
+}
+
+function validateUrl(value) {
+  return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(
+    value
+  );
 }
 
 const button = document.getElementById("hello");
